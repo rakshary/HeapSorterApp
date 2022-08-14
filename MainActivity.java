1722830cf17d14615910cf2639a801c06921a3fe
rakshary <93668440+rakshary@users.noreply.github.com>
2022-08-14 23:04:11 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.heapsorting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+        private TextView textView;
+        private EditText editText;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+    }
+
+        public void SortButton(View view) {
+        String[] stringNumber = editText.getText().toString().split(",");
+        Integer[] integerNumber = new Integer[stringNumber.length];
+
+        for (int i = 0; i < stringNumber.length; i++) {
+            integerNumber[i] = Integer.parseInt(stringNumber[i]);
+        }
+        Integer[] sortedNumbers = HeapSort(integerNumber);
+        textView.setText(Arrays.toString(sortedNumbers));
+
+    }
+
+        private Integer[] HeapSort(Integer[] arr) {
+        int n = arr.length;
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
+        private void heapify(Integer arr[], int n, int i)
+        {
+            int largest = i; // Initialize largest as root
+            int l = 2 * i + 1; // left = 2*i + 1
+            int r = 2 * i + 2; // right = 2*i + 2
+
+            // If left child is larger than root
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            // If right child is larger than largest so far
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            // If largest is not root
+            if (largest != i) {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                // Recursively heapify the affected sub-tree
+                heapify(arr, n, largest);
+            }
+        }
+
+    }
